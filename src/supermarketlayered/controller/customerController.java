f8067baff617fcf88c21f491a175d2c99dcef5a4
Nethmi-24 <batawalann.20@uom.lk>
2023-08-06 11:07:35 +0530

    first

---
+++ src/supermarketlayered/controller/customerController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supermarketlayered.controller;
+
+/**
+ *
+ * @author Nethmi
+ */
+public class customerController {
+    public String saveCustomer(){
+    return null;    
+    }
+}
---
+++ src/supermarketlayered/service/custom/impl/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supermarketlayered.service.custom.impl;
+
+import supermarketlayered.dto.CustomerDto;
+import supermarketlayered.service.custom.CustomerService;
+
+/**
+ *
+ * @author Nethmi
+ */
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public String addCustomer(CustomerDto customerDto)throws Exception{
+        return null;
+    }
+}
